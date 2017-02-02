90508f595ceefe9c75d70457a68f7a156511e441
USER2 <USER2>
2017-02-02 23:32:26 +0900

    Add swipe for mobile touch

--- js/vibration.js
+++ js/vibration.js
@@ -11,30 +11,39 @@ if (navigator.vibrate) {
 } else {
   console.log('no vibration for you :-(');
 }
+
+$(document).on("pagecreate","#pageone",function(){
+  $("button").on("swipe",function(){
+    $(this).getElementById('vib-bar-100').addEventListener('click',function(){
+      navigator.vibrate(100);
+      // window.navigator.vibrate(1000);
+    });
+  });
+});
 // window.navigator = window.navigator || {};
 // navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-document.getElementById('vib-bar-100').addEventListener('click',function(){
+// document.getElementById('vib-bar-100').addEventListener('click',function(){
+//   navigator.vibrate(100);
+//   // window.navigator.vibrate(1000);
+// });
+
+document.getElementById('vib-bar-200').addEventListener('click',function(){
   navigator.vibrate(100);
   // window.navigator.vibrate(1000);
 });
 
-document.getElementById('vib-bar-200').addEventListener('click',function(){
-  navigator.vibrate(200);
-  // window.navigator.vibrate(1000);
-});
-
 document.getElementById('vib-bar-300').addEventListener('click',function(){
-  navigator.vibrate(300);
+  navigator.vibrate(100);
   // window.navigator.vibrate(1000);
 });
 
 document.getElementById('vib-bar-400').addEventListener('click',function(){
-  navigator.vibrate(400);
+  navigator.vibrate(100);
   // window.navigator.vibrate(1000);
 });
 
 document.getElementById('vib-bar-500').addEventListener('click',function(){
-  navigator.vibrate(500);
+  navigator.vibrate(100);
   // window.navigator.vibrate(1000);
 });
       // if (navigator.vibrate === undefined) {
