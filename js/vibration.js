36cc5e2bf55792e33d013478becc1f3df5be367f
USER2 <USER2>
2017-02-02 23:38:21 +0900

    Test swipe

--- js/vibration.js
+++ js/vibration.js
@@ -14,11 +14,9 @@ if (navigator.vibrate) {
 
 $(document).on("pagecreate","#pageone",function(){
   $("button").on("swipe",function(){
-    $(this).getElementById('vib-bar-100').addEventListener('click',function(){
       navigator.vibrate(100);
       // window.navigator.vibrate(1000);
     });
-  });
 });
 // window.navigator = window.navigator || {};
 // navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
@@ -27,25 +25,25 @@ $(document).on("pagecreate","#pageone",function(){
 //   // window.navigator.vibrate(1000);
 // });
 
-document.getElementById('vib-bar-200').addEventListener('click',function(){
-  navigator.vibrate(100);
-  // window.navigator.vibrate(1000);
-});
-
-document.getElementById('vib-bar-300').addEventListener('click',function(){
-  navigator.vibrate(100);
-  // window.navigator.vibrate(1000);
-});
-
-document.getElementById('vib-bar-400').addEventListener('click',function(){
-  navigator.vibrate(100);
-  // window.navigator.vibrate(1000);
-});
-
-document.getElementById('vib-bar-500').addEventListener('click',function(){
-  navigator.vibrate(100);
-  // window.navigator.vibrate(1000);
-});
+// document.getElementById('vib-bar-200').addEventListener('click',function(){
+//   navigator.vibrate(100);
+//   // window.navigator.vibrate(1000);
+// });
+//
+// document.getElementById('vib-bar-300').addEventListener('click',function(){
+//   navigator.vibrate(100);
+//   // window.navigator.vibrate(1000);
+// });
+//
+// document.getElementById('vib-bar-400').addEventListener('click',function(){
+//   navigator.vibrate(100);
+//   // window.navigator.vibrate(1000);
+// });
+//
+// document.getElementById('vib-bar-500').addEventListener('click',function(){
+//   navigator.vibrate(100);
+//   // window.navigator.vibrate(1000);
+// });
       // if (navigator.vibrate === undefined) {
       //     document.getElementById('v-unsupported').classList.remove('hidden');
       //     ['button-play-v-once', 'button-play-v-thrice', 'button-stop-v'].forEach(function(elementId) {
